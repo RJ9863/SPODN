a9fabf5fffa3a2bcec4f0b8d7beda96ff7fca315
RJ9863 <rj9863@student.uni-lj.si>
2025-11-16 23:53:27 +0100

    rec implemented

--- ass1/rec.asm
+++ ass1/rec.asm
@@ -12,9 +12,13 @@ starti	JSUB	readc
 		MULR	T,S
 		ADDR	S,A
 		STA		sum
+		J		starti
 		
 fact	LDA		sum
 		JSUB	factl
+		JSUB	print
+		J		startn
+		
 waitout	TD		stdout
 		JEQ		waitout
 		WD		stdout
@@ -24,15 +28,30 @@ factl	COMP	#1
 		JEQ		fret1
 		COMP	#0
 		JEQ		fret1
-		LDX		STI
-		STA		STACK,X
-		LDB		#3
-		ADDR	B,X
-		STX		STI		
+		JSUB	push
+		LDB		#1
+		SUBR	B,A
 		JSUB	factl
+		LDB		#0
+		ADDR	A,B
+		JSUB	pop
 		MULR	B,A
 		RSUB
 fret1	LDA		#1
+		RSUB
+		
+push	STA		STACK,X
+		STB		bsave
+		LDB		#3
+		ADDR	B,X
+		LDB		bsave
+		RSUB
+		
+pop		LDA	STACK,X
+		STB		bsave
+		LDB		#3
+		SUBR	B,X
+		LDB		bsave
 		RSUB
 		
 halt	J      	halt
@@ -41,11 +60,39 @@ readc	TD		devin
 		JEQ		readc
 		RD		devin
 		RSUB
+print	JSUB	dmod10
+		LDA		#0
+		ADDR	S, A
+		JSUB	push
+		LDA		#0
+		ADDR	T, A
+		COMP	#0
+		JEQ		retlbl
+		JSUB 	pop
+		LDB		#0x30
+		ADDR	B, A
+		JSUB	printc
+		RSUB
+		
+dmod10	LDS		#0
+		LDT		#0
+		ADDR	A, S
+		ADDR	A, T
+		LDB		#10
+		DIVR	B, S
+		MULR	S, B
+		SUBR	B, S
+		RSUB
+		
+printc	TD		stdout
+		JEQ		printc
+		WD		stdout
 . data
+bsave	WORD	0
 STACK	RESW	100
 sum		WORD	0
 devin	BYTE	0xFA
 stdout	BYTE	0x01
 zero	WORD	0x31
 null	WORD	0x00
-		END    	waitin
+		END    	init
